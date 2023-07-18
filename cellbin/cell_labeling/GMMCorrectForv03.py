03578ffedf0eaf3bcf074875df0bd37793ab14de
liuhuanlin <liuhuanlin@genomics.cn>
2023-07-18 10:13:44 +0800

    a. update GMM

--- cellbin/cell_labeling/GMMCorrectForv03.py
+++ cellbin/cell_labeling/GMMCorrectForv03.py
@@ -16,7 +16,6 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.mixture import GaussianMixture
 from warnings import filterwarnings
-from multiprocessing import Pool
 
 filterwarnings('ignore')
 
@@ -73,22 +72,7 @@ class CellCorrection(object):
         mask = tifi.imread(self.mask_file)
         return gem, mask
 
-    def _correction(self, cell_df, bg_df, cell_id):
-        try:
-            clf = GaussianMixture(n_components=3, covariance_type='spherical')
-            clf.fit(cell_df[['x', 'y', 'MIDCount']].values)
-            bg_group = bg_df.groupby(['x', 'y']).agg(MID_max=('MIDCount', 'max')).reset_index()
-            cell_test_bg = pd.merge(bg_df, bg_group, on=['x', 'y'])
-            score = pd.Series(-clf.score_samples(cell_test_bg[['x', 'y', 'MID_max']].values))
-            cell_test_bg['CellID'] = np.where(score < self.threshold, cell_id, 0)
-            cell_test_bg.drop('MID_max', axis=1, inplace=True)
-            cell_test_bg = cell_test_bg[cell_test_bg['CellID'] == cell_id]
-            cell_gmm_df = pd.concat([cell_test_bg, cell_df])
-            return cell_gmm_df
-        except:
-            return cell_df
-
-    def _GMM_correction(self, gem, mask, pool):
+    def _GMM_correction(self, gem, mask):
         num_labels, maskImg, _, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
         centroids = centroids[1:].astype(int)
         max_y, min_y, max_x, min_x = centroids[:, 1].max(), centroids[:, 1].min(), centroids[:, 0].max(), centroids[:,
@@ -102,7 +86,6 @@ class CellCorrection(object):
         del gem, maskImg
 
         gmm_gem = []
-        proces = []
         for cell_id, (x, y) in tqdm(enumerate(centroids)):
             try:
                 cell_id = cell_id + 1
@@ -111,17 +94,20 @@ class CellCorrection(object):
                 cell_df = cell_df[cell_df['CellID'] == cell_id]
                 bg_df = bg_gem[(bg_gem['x'] < x + self.radius) & (bg_gem['x'] > x - self.radius) & (
                             bg_gem['y'] < y + self.radius) & (bg_gem['y'] > y - self.radius)]
-                p = pool.apply_async(self._correction, (cell_df, bg_df, cell_id))
-                proces.append(p)
+                clf = GaussianMixture(n_components=3, covariance_type='spherical')
+                clf.fit(cell_df[['x', 'y', 'MIDCount']].values)
+                bg_group = bg_df.groupby(['x', 'y']).agg(MID_max=('MIDCount', 'max')).reset_index()
+                cell_test_bg = pd.merge(bg_df, bg_group, on=['x', 'y'])
+                score = pd.Series(-clf.score_samples(cell_test_bg[['x', 'y', 'MID_max']].values))
+                cell_test_bg['CellID'] = np.where(score < self.threshold, cell_id, 0)
+                cell_test_bg.drop('MID_max', axis=1, inplace=True)
+                cell_test_bg = cell_test_bg[cell_test_bg['CellID'] == cell_id]
+                cell_gmm_df = pd.concat([cell_test_bg, cell_df])
+                gmm_gem.append(cell_gmm_df)
                 # if cell_id==100:
                 #     break
             except Exception as e:
                 print(e)
-        pool.close()
-        pool.join()
-        for p in proces:
-            if p.get() is not None:
-                gmm_gem.append(p.get())
 
         correct_data = pd.concat(gmm_gem)
         correct_data = correct_data.drop_duplicates(subset=['geneID', 'x', 'y', 'MIDCount'], keep='first')
@@ -129,14 +115,13 @@ class CellCorrection(object):
         correct_data.to_csv(os.path.join(self.out_path, 'cell_mask_profile.txt'), sep='\t', index=False)
 
     def cell_correct(self, ):
-        pool = Pool(processes=3)
         t0 = time.time()
         gem, mask = self.__creat_gxp_data()
         print(gem.head())
         t1 = time.time()
         print('Load data :', (t1 - t0))
         t2 = time.time()
-        self._GMM_correction(gem, mask, pool)
+        self._GMM_correction(gem, mask)
         t3 = time.time()
         print('Correct :', (t3 - t2))
         print('Total :', (t3 - t0))
@@ -145,9 +130,8 @@ class CellCorrection(object):
 def args_parse():
     usage = """ Usage: %s Cell expression file (with background) path, multi-process """
     arg = argparse.ArgumentParser(usage=usage)
-    arg.add_argument('-m', '--mask_path', help='cell mask',
-                     default=r"C:\Users\wuyiwen\Desktop\SS200000135TL_D1_nuclei_mask.tif")
-    arg.add_argument('-g', '--gem_path', help='gem file', default=r"C:\Users\wuyiwen\Desktop\SS200000135TL_D1.gem.gz")
+    arg.add_argument('-m', '--mask_path', help='cell mask', default=r"SS200000213BR_C5_nuclei_mask.tif")
+    arg.add_argument('-g', '--gem_path', help='gem file', default=r"SS200000213BR_C5.gem.gz")
     arg.add_argument('-o', '--out_path', help='output path', default='./')
     arg.add_argument('-p', '--process', help='n process', type=int, default=1)
     arg.add_argument('-t', '--threshold', help='threshold', type=int, default=20)
